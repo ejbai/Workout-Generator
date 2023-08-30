f7cd6a626378cbd2b563d8bee7f1a738994f632e
ejbai <111667032+ejbai@users.noreply.github.com>
2023-08-30 16:52:16 -0400

    Add files via upload

---
+++ WorkoutGenerator.py
@@ -0,0 +1,67 @@
+import random
+
+
+# Create push array
+
+pushArray = []
+pushFile = open("C:/Users/flybr/Desktop/Python Projects/WorkoutGenerator/PushExercises.txt")
+
+x = 0   # Start the count for the index of the array in the following loop
+for currentLine in pushFile:
+    pushArray.append(currentLine)
+    pushArray[x] = pushArray[x].strip()
+    x += 1
+
+
+# Create pull array
+
+pullArray = []
+pullFile = open("C:/Users/flybr/Desktop/Python Projects/WorkoutGenerator/PullExercises.txt")
+
+x = 0   # Start the count for the index of the array in the following loop
+for currentLine in pullFile:
+    pullArray.append(currentLine)
+    pullArray[x] = pullArray[x].strip()
+    x += 1
+
+
+# Create leg array
+
+legArray = []
+legFile = open("C:/Users/flybr/Desktop/Python Projects/WorkoutGenerator/LegExercises.txt")
+
+x = 0   # Start the count for the index of the array in the following loop
+for currentLine in legFile:
+    legArray.append(currentLine)
+    legArray[x] = legArray[x].strip()
+    x += 1
+
+
+def createWorkout():
+
+    MondayExercises = random.sample(pushArray, 8)   # Push
+    MondayString = ', '.join(MondayExercises)
+
+    TuesdayExercises = random.sample(pullArray, 8)  # Pull
+    TuesdayString = ', '.join(TuesdayExercises)
+   
+    WednesdayExercises = random.sample(legArray, 8) # Leg
+    WednesdayString = ', '.join(WednesdayExercises)
+   
+    ThursdayExercises = random.sample(pushArray, 8) # Push
+    ThursdayString = ', '.join(ThursdayExercises)
+   
+    FridayExercises = random.sample(pullArray, 8)   # Push
+    FridayString = ', '.join(FridayExercises)
+   
+    SaturdayExercises = random.sample(legArray, 8)  # Leg
+    SaturdayString = ', '.join(SaturdayExercises)
+
+    print("Monday: ", MondayString, "\n")
+    print("Tuesday: ", TuesdayString, "\n")
+    print("Wednesday: ", WednesdayString, "\n")
+    print("Thursday: ", ThursdayString, "\n")
+    print("Friday: ", FridayString, "\n")
+    print("Saturday: ", SaturdayString, "\n")
+
+createWorkout()
